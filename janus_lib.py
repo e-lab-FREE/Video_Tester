2b4df7e56e36ab2363e38d1cd1ede4936d697cbd
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-11-09 13:12:36 +0000

    video Server File

---
+++ janus_lib.py
@@ -0,0 +1,173 @@
+import json
+import requests
+from pprint import pprint
+plugin_id = 0
+session_id = 0
+#server = "elab1.ist.utl.pt"
+serverl = "localhost"
+def connect_janus_stream(apisecret):
+    
+    data = {
+    "janus": "create",
+    "transaction": "transaction-x",
+    "apisecret" : str(apisecret)
+    }
+    print(f'http://{server}:8088/janus/')
+    response = requests.post(f'http://{server}:8088/janus/', json = data)
+    #print(response)
+
+    json_ret = response.json()
+
+    if(response.status_code != 200 or  json_ret['janus']!='success'):
+        print("ERROR")
+        print(response.json())
+        return (0, 0)
+
+    #get session_id
+    session_id = json_ret['data']['id']
+
+    #print(session_id)
+
+    #connect to streamming plugin
+
+    data = {
+    "janus" : "attach",
+    "plugin" : "janus.plugin.streaming",
+    "transaction": "transaction-x",
+    "apisecret": "janusrocks"
+    }
+
+    response = requests.post(f'http://{server}:8088/janus/'+str(session_id), json = data)
+    json_ret = response.json()
+    if(response.status_code != 200 or  json_ret['janus']!='success'):
+        print("ERROR")
+        print(response.json())
+        return (0, 0)
+
+#    print(response.json())
+#   print('OK')
+
+    plugin_id = json_ret['data']['id']
+    #print("plugin_id "+str(plugin_id))
+    return   (plugin_id, session_id)
+
+def list_streams(apisecret):
+    plugin_id, session_id = connect_janus_stream(apisecret)
+    ret_list = []
+    #print(session_id)
+    #print(plugin_id)
+
+    if session_id ==0 or plugin_id == 0:
+        return -1
+
+    data = {
+        "janus": "message",
+        "body": {
+            "request": "list"
+        },
+        "transaction": "transaction-x",
+        "apisecret": apisecret
+    }
+
+    #print(f'http://localhost:8088/janus/{session_id}/{plugin_id}'    )
+    response = requests.post(f'http://{server}:8088/janus/{session_id}/{plugin_id}', json = data)
+    json_ret = response.json()
+    try:
+        return json_ret['plugindata']['data']['list']
+    except:
+        return []
+    
+
+
+def stream_info(apisecret, stream_id):
+    plugin_id, session_id = connect_janus_stream()
+    ret_list = []
+    #print(session_id)
+    #print(plugin_id)
+
+    if session_id ==0 or plugin_id == 0:
+        return []
+
+    data = {
+            "janus": "message",
+            "body": {
+                "request": "info",
+                "id": int(stream_id)
+            },
+            "transaction": "transaction-x",
+            "apisecret": apisecret
+        }
+    #print(f'http://localhost:8088/janus/{session_id}/{plugin_id}'    )
+    response = requests.post(f'http://{server}:8088/janus/{session_id}/{plugin_id}', json = data)
+    json_ret = response.json()
+    try: 
+        return json_ret['plugindata']['data']['info']
+    except:
+        return {}
+
+
+
+
+def create_stream(name, description, port, apisecret):
+    plugin_id, session_id = connect_janus_stream()
+
+    if session_id ==0 or plugin_id == 0:
+        return {}
+
+    data = {
+        "janus": "message",
+        "body": {
+            
+                "request" : "create",
+                #"admin_key" : "<plugin administrator key; mandatory if configured>",
+                "type" : "rtp",
+                #"id" : <unique ID to assign the mountpoint; optional, will be chosen by the server if missing>,
+                "name" : name,
+                "description" : description,
+                #"metadata" : "<metadata of mountpoint; optional>",
+                #"secret" : "<secret to query/edit the mountpoint later; optional>",
+                #"pin" : "<PIN required for viewers to access mountpoint; optional>",
+                #"is_private" : <true|false, whether the mountpoint should be listable; true by default>,
+                "audio" : False,
+                "video" : True,
+                #"data" : <true|false, whether the mountpoint will have datachannels; false by default>,
+                "permanent" : True,
+                "videoport" : int(port),  #Porto onde são recebidos os pacotes do stream (ver comando de gstreamer) 
+                "videopt" : 126,  #nao alterar 
+                "videortpmap" : "H264/90000",  #nao alterar 
+                "videofmtp" : "profile-level-id=42e01f;packetization-mode=1" #nao alterar 
+            
+        },
+        "transaction": "transaction-x",
+        "apisecret": apisecret
+    }
+
+    #print(f'http://localhost:8088/janus/{session_id}/{plugin_id}')
+    response = requests.post(f'http://{server}:8088/janus/{session_id}/{plugin_id}', json = data)
+    json_ret = response.json()
+    if(json_ret['janus']!= 'success'):
+        return {}
+    json_plugin = json_ret['plugindata']['data']
+    try: 
+        return json_plugin['stream'] 
+    except:
+        print(json_plugin['error'])
+        return {}
+ 
+
+if __name__ == "__main__":
+    #secret = input('Janus Secret ')
+    secret = "janusrocks"
+    while True:
+        print("L - List Streams\nN - New Strem\nD - Delete Stream\nI - Stream Info")
+        option = input('L I N D :')
+        if option == 'L':
+            pprint(list_streams(secret))
+        if option == 'I':
+            stream_id = input("Stream ID :")
+            pprint(stream_info(secret, stream_id))
+        if option == 'N':
+            s_name = input("Stream name :")
+            s_description = input("Stream description :")
+            s_port = input("Stream port :")
+            pprint(create_stream(s_name, s_description, s_port, "janusrocks"))
---
+++ janus_view_app.py
@@ -0,0 +1,33 @@
+import os
+from janus_lib import *
+
+from flask import Flask, jsonify, render_template
+from threading import Thread
+
+secret = "janusrocks"
+app = Flask(__name__, instance_relative_config=True, 
+            static_url_path='', static_folder='static')
+   
+
+
+@app.route('/')
+@app.route('/list')
+def list():
+    ret_list = []
+    ls = list_streams(secret)
+    for s in ls:
+        ret_list.append(stream_info(secret, s['id']))
+    return render_template("video_list.html", stream_list = ret_list)
+    return jsonify(ret_list)
+
+@app.route('/video/<video_id>')
+def video(video_id):
+    return render_template("video.html", video_id = video_id)
+@app.route('/video_simple/<video_id>')
+def video_simple(video_id):
+    return render_template("video_simple.html", video_id = video_id)
+app.run(debug=True, host= '0.0.0.0', port= 5000)
+
+#   Preparing parameters for flask to be given in the thread
+#   so that it doesn't collide with main thread
+
